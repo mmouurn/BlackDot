30a7f6bd4bdf36ce2f3f02c7b939f7c4d41619ec
mmourn <101750906+mmouurn@users.noreply.github.com>
2022-12-23 19:19:31 +0000

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/mmouurn/Loader2/main/JN%23hx%242rDapKZVgA'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/mmouurn/Loader/main/Loader'))()
